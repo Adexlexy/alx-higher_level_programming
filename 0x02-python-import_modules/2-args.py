efc6e26ca2ce46dd0fa01c9ab4b63d0b19699d3b
Adexlexy <oshewolo_martins@yahoo.com>
2023-01-06 13:22:48 -0800

    Python Import Module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
              print("{} argument:".format(total - 1))
          else:
              print("{} arguments:".format(total - 1))
-             for i in range(1, total):
-                 print("{}: {}".format(i, sys.argv[i]))
+         for i in range(1, total):
+             print("{}: {}".format(i, sys.argv[i]))
