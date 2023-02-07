f9a0a66c7655f5ba059c3c67b767b5354af83633
Adexlexy <oshewolo_martins@yahoo.com>
2023-02-07 07:28:15 -0800

    Adexlexy

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@ def read_file(filename=""):
     """contains the read file functions"""
 
     with open("filename", encoding="utf-8") as filename:
-        """the code for opening and reading a file"""
+        """reads a text file(UTF8) and prints it to stdout"""
 
         print(filename.read(), end ="")
