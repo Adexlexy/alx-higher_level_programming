10e1879ced168264ad958672c45e948e9caf681e
Adexlexy <oshewolo_martins@yahoo.com>
2023-02-07 07:13:57 -0800

    Python input and output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """contains the read file functions"""
+
+    with open("filename", encoding="utf-8") as myFile:
+        """the code for opening and reading a file"""
+
+    print(myFile.read())
+
