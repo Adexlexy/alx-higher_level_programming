cfdf259355de110e492ad69ef7e43060ca5dfa7c
Adexlexy <oshewolo_martins@yahoo.com>
2023-02-08 01:22:16 -0800

    Python Input and Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 """
 contains the functions "write file"
 """
 
 
 def write_file(filename="", text=""):
-    """returns the number of char written to "filename" from "text" """ 
+    """returns the number of chars written to "filename" from "text" """ 
     with open(filename,'w', encoding='utf=8') as myFile:
         return myFile.write(text)
