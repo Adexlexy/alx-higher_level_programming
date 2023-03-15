e2b05eede57ba594525adf7d46cdc12d08b5aa71
Adexlexy <oshewolo_martins@yahoo.com>
2023-03-15 05:56:56 -0700

    intro to SQL

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- a script that creates the database hbtn_0c_0
+-- Script should not fail if thed database already exist
+CREATE SCHEMA IF NOT EXIST hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- script that creates a table called first_table in the current database
+CREATE TABLES [IF NOT EXIST] first_table(
+	first_tableID int,
+	name VARCHAR(256));
