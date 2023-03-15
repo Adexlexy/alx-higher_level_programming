5f8a8f5dc58eff448dbf10ab57e78ba8016e6d97
Adexlexy <oshewolo_martins@yahoo.com>
2023-03-15 08:08:03 -0700

    INTRO TO SQL

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- script that deletes the database hbtn_0c_0 in your MySQL server
 -- if the database hbtn_0c_0 exist,script will not fail
-DROP SCHEMA [IF EXISTS] hbtn_0c_0;
+DROP SCHEMA [IF EXISTS] `hbtn_0c_0`;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- script that creates a table called first_table in the current database
-CREATE TABLES [IF NOT EXIST] first_table(
-	first_tableID int,
-	name VARCHAR(256));
+CREATE TABLES [IF NOT EXISTS] first_table(
+	`id` INT,
+	`name` VARCHAR(256));
